e5d54cf9e25b88ca8001cf012e99c42cef15662d
Anna Olson <olsona@cs.uchicago.edu>
2014-06-26 18:08:08 -0600

    Added network dynamics code

--- aps/graphsAPS.py
+++ aps/graphsAPS.py
@@ -1,12 +1,16 @@
 import parseAPS
+import networkx as nx
 
-def getAdjListSimple(df, what='authors', authorInitialsOnly=False, subsetPACS=None):
+def getAdjListSimple(df, what='authors', authorInitialsOnly=False, subsetPACS=None, subsetYears=None):
 	result = {}
+	aio = authorInitialsOnly
+	sp = subsetPACS
+	sy = subsetYears
 	for index, row in df.iterrows():
 		if what == 'authors':
-			items = parseAPS.getAuthors(row, authorInitialsOnly, subsetPACS)
-		elif what == 'categories':
-			items = parseAPS.getPacs(row, subsetPACS)
+			items = parseAPS.getAuthors(row, authorInitialsOnly=aio, subsetPACS=sp, subsetYears=sy)
+		elif what == 'pacs':
+			items = parseAPS.getPACS(row, subsetPACS=sp, subsetYears=sy)
 		if items:  # Skip this row if items is None
 			lead = items[0]
 			if lead not in result:
@@ -22,16 +26,20 @@ def getAdjListSimple(df, what='authors', authorInitialsOnly=False, subsetPACS=No
 	return result
 
 
-def getAdjListBipartite(df, authorInitialsOnly=False, subsetPACS=None):
+def getAdjListBipartite(df, authorInitialsOnly=False, subsetPACS=None, subsetYears=None):
 	result = {}
+	aio = authorInitialsOnly
+	sp = subsetPACS
+	sy = subsetYears
 	for index, row in df.iterrows():
-		auths = parseAPS.getAuthors(row, authorInitialsOnly, subsetPACS)
-		pacs = parseAPS.getPacs(row, subsetPACS)
+		auths = parseAPS.getAuthors(row, authorInitialsOnly=aio, subsetPACS=sp, subsetYears=sy)
+		pacs = parseAPS.getPACS(row, subsetPACS=sp, subsetYears=sy)
 		if auths and pacs:
 			for a in auths:
 				if a not in result:
 					result[a] = {}
-					result[a][p] = {'weight': 1}
+					for p in pacs:
+						result[a][p] = {'weight': 1}
 				else:
 					for p in pacs:
 						if p not in result[a]:
@@ -41,18 +49,78 @@ def getAdjListBipartite(df, authorInitialsOnly=False, subsetPACS=None):
 	return result
 
 
-def makeGraph(df, authorInitialsOnly=False, subsetPACS=None, what=['authors']):
-	import networkx as nx 
-	w = what
+def makeGraph(df, authorInitialsOnly=False, subsetPACS=None, subsetYears=None, what=['authors']):
 	aio = authorInitialsOnly
 	sp = subsetPACS
+	sy = subsetYears
+	w = what
 	if len(what) == 1:
-		adjList = getAdjListSimple(df, authorInitialsOnly=aio, subsetPACS=sp, what=w[0])
+		adjList = getAdjListSimple(df, authorInitialsOnly=aio, subsetPACS=sp, subsetYears=sy, what=w[0])
 	elif len(what) == 2:
-		adjList = getAdjListBipartite(df, authorInitialsOnly=aio, subsetPACS=sp)
+		adjList = getAdjListBipartite(df, authorInitialsOnly=aio, subsetYears=sy, subsetPACS=sp)
 
 	if adjList:
 		G = nx.from_dict_of_dicts(adjList)
 		return G
 	else:
 		return None
+		
+		
+def getDynamicNetwork(df, what='authors', authorInitialsOnly=False, subsetPACS=None, startYear=1982, endYear=2008, window=5):
+	diam = window/2
+	yearSet = {y:range(y-diam,y+diam+1) for y in range(startYear,endYear+1)}
+	results = {y:{} for y in yearSet}
+	aio = authorInitialsOnly
+	sp = subsetPACS
+	for index, row in df.iterrows():
+		if what == 'authors':
+			ret = parseAPS.getAuthorsYears(row, authorInitialsOnly=aio, subsetPACS=sp)
+		elif what == 'pacs':
+			ret = parseAPS.getPACSYears(row, subsetPACS=sp)
+		[year, items] = ret
+		if items:  # Skip this row if items is None
+			for yearKey in yearSet.keys():
+				yearRange = yearSet[yearKey]
+				if year in yearRange:
+					lead = items[0]
+					if lead not in results:
+						results[yearKey][lead] = {}
+						for follow in items[1:]:
+							results[yearKey][lead][follow] = {'weight': 1}
+					else:
+						for follow in items[1:]:
+							if follow not in result[lead]:
+								results[yearKey][lead][follow] = {'weight': 1}
+							else:
+								results[yearKey][lead][follow]['weight'] += 1
+	return results
+	
+	
+def makeDynamicGraphs(df, what='authors', authorInitialsOnly=False, subsetPACS=None, startYear=1982, endYear=2008, window=5, stats=['edges']):
+	aio = authorInitialsOnly
+	sp = subsetPACS
+	sy = startYear
+	ey = endYear
+	wi = window
+	w = what
+	adjLists = getDynamicNetwork(df, what=w, authorInitialsOnly=aio, subsetPACS=sp, startYear=sy, endYear=ey, window=wi)
+	numY = len(adjLists)
+	print numY
+	statsLists = {st:[0.0]*numY for st in stats}
+	for yearKey in sorted(adjLists.keys()):
+		graphDict = adjLists[yearKey]
+		G = nx.from_dict_of_dicts(graphDict)
+		for st in statsLists.keys():
+			if st in ['edges','nodes']:
+				statsLists[st][yearKey-startYear] = len(statsDict[st](G))
+			else:
+				statsLists[st][yearKey-startYear] = statsDict[st](G)
+	return statsLists
+	
+	
+# because I love dispatch tables	
+statsDict = {'edges':nx.edges,
+			'nodes':nx.nodes,
+			'degree_assortativity':nx.degree_pearson_correlation_coefficient,
+			'components':nx.number_connected_components
+			}
--- aps/parseAPS.py
+++ aps/parseAPS.py
@@ -1,98 +1,205 @@
 from collections import OrderedDict
 
 def xml2pickle(infile,outfile):
-    import pandas as pd
-    import xmltodict
-    import cPickle as pickle
+	import pandas as pd
+	import xmltodict
+	import cPickle as pickle
 
-    # read and process infile
-    f = open(infile,'r')
-    d = xmltodict.parse(f)
-    df = pd.DataFrame(d['articles']['article'])
-    df = df.dropna(subset=['authgrp','pacs'])
-    pickle.dump(df,open(outfile,'wb'))
+	# read and process infile
+	f = open(infile,'r')
+	d = xmltodict.parse(f)
+	df = pd.DataFrame(d['articles']['article'])
+	df = df.dropna(subset=['authgrp','pacs'])
+	pickle.dump(df,open(outfile,'wb'))
 
 
-def getAuthors(row, initials_only=False, pacsList=None):
-    authgrp = row['authgrp'] 
-    if pacsList:
-        numGood = 0
-        paperPacs = row['pacs']['pacscode']
-        for pp in paperPacs:
-            if pp in pacsList:
-                numGood += 1
-        if numGood:
-            authorList = processAuthors(authgrp, initials_only)
-        else:
-            authorList = []
-    else:
-        authorList = processAuthors(authgrp, initials_only)
-    return authorList
-    
+def getAuthors(row, authorInitialsOnly=False, subsetPACS=None, subsetYears=None):
+	# subsetYears first looks at the received date, and if that data isn't available, then at published date, and then at revised date.
+	authgrp = row['authgrp'] 
+	goodP = 0
+	goodY = 0
+	if subsetYears:
+		year = getYear(row)
+		if year in subsetYears:
+			goodY = 1
+	else:
+		goodY = 1
+	if subsetPACS and goodY:
+		numGood = 0
+		subsetPACS = row['pacs']['pacscode']
+		for pp in subsetPACS:
+			if pp in subsetPACS:
+				numGood += 1
+		if numGood:
+			goodP = 1
+	else:
+		goodP = 1
+	if goodP and goodY:
+		authorList = processAuthors(authgrp, authorInitialsOnly)
+	else:
+		authorList = []
+	return authorList
+	
+	
+def getAuthorsYears(row, authorInitialsOnly=False, subsetPACS=None):
+	# subsetYears first looks at the received date, and if that data isn't available, then at published date, and then at revised date.
+	authgrp = row['authgrp'] 
+	goodP = 0
+	year = getYear(row)
+	if subsetPACS:
+		numGood = 0
+		subsetPACS = row['pacs']['pacscode']
+		for pp in subsetPACS:
+			if pp in subsetPACS:
+				numGood += 1
+		if numGood:
+			goodP = 1
+	else:
+		goodP = 1
+	if goodP:
+		authorList = processAuthors(authgrp, authorInitialsOnly)
+	else:
+		authorList = []
+	return [year, authorList]
+	
 
-def processAuthors(authgrp, initials_only=False):
-    from collections import OrderedDict
-    authorList = []
-    if type(authgrp) is OrderedDict:
-        if 'author' in authgrp.keys():
-            author = authgrp['author']
-        else:
-            author = authgrp
-        if type(author) is OrderedDict:
-            if 'surname' in author.keys():
-                if 'givenname' in author.keys():
-                    gname = author['givenname']
-                    if initials_only:
-                        gname = gname[0]
-                else:
-                    gname = "-"
-                if 'middlename' in author.keys():
-                    mname = author['middlename']
-                    if type(mname) is list and not initials_only:
-                        mname = tuple(mname)
-                    elif initials_only:
-                        mname = mname[0]
-                else:
-                    mname = "-"
-                if 'suffix' in author.keys():
-                    suff = author['suffix']
-                else:
-                    suff = "-"
-                name = (gname,mname,author['surname'],suff)
-                authorList.append(name)
-        else:
-            for a in author:
-                alist = processAuthors(a)
-                for al in alist:
-                    authorList.append(al)
-    else:
-        for a in authgrp:
-            alist = processAuthors(a)
-            for al in alist:
-                authorList.append(al)
-    return authorList
-    
+def processAuthors(authgrp, authorInitialsOnly=False):
+	from collections import OrderedDict
+	authorList = []
+	if type(authgrp) is OrderedDict:
+		if 'author' in authgrp.keys():
+			author = authgrp['author']
+		else:
+			author = authgrp
+		if type(author) is OrderedDict:
+			if 'surname' in author.keys():
+				if 'givenname' in author.keys():
+					gname = author['givenname']
+					if authorInitialsOnly:
+						gname = gname[0]
+				else:
+					gname = "-"
+				if 'middlename' in author.keys():
+					mname = author['middlename']
+					if type(mname) is list and not authorInitialsOnly:
+						mname = tuple(mname)
+					elif authorInitialsOnly:
+						mname = mname[0]
+				else:
+					mname = "-"
+				if 'suffix' in author.keys():
+					suff = author['suffix']
+				else:
+					suff = "-"
+				name = (gname,mname,author['surname'],suff)
+				authorList.append(name)
+		else:
+			for a in author:
+				alist = processAuthors(a)
+				for al in alist:
+					authorList.append(al)
+	else:
+		for a in authgrp:
+			alist = processAuthors(a)
+			for al in alist:
+				authorList.append(al)
+	return authorList
+	
 
-def getPACS(row, pacsList=None):
-    paperPacs = row['pacs']['pacscode']
-    if pacsList:
-        goodList = []
-        for pp in paperPacs:
-            if pp in pacsList:
-                goodList.append(pp)
-        return goodList
-    else:
-        return pac
+def getPACS(row, subsetPACS=None, subsetYears=None):
+	paperPacs = convertPACS(row['pacs']['pacscode'])
+	goodY = 0
+	if subsetYears:
+		year = getYear(row)
+		if year in subsetYears:
+			goodY = 1
+	else:
+		goodY = 1
+		
+	if subsetPACS and goodY:
+		goodList = []
+		for pp in paperPacs:
+			if pp in subsetPACS:
+				goodList.append(pp)
+	elif goodY:
+		goodList = paperPacs
+	else:
+		goodList = []
+		
+	return goodList
+
+
+def getPACSYears(row, subsetPACS=None):
+	paperPacs = convertPACS(row['pacs']['pacscode'])
+	year = getYear(row)
+		
+	if subsetPACS:
+		goodList = []
+		for pp in paperPacs:
+			if pp in subsetPACS:
+				goodList.append(pp)
+	else:
+		goodList = paperPacs
+	
+	return [year, goodList]
+
+
+def convertPACS(pacsList,pacsLevel=2):
+	if type(pacsList) is not list:
+		pacsList = [pacsList]
+	if pacsLevel == 1:
+		newList = []
+		for p in pacsList:
+			try:
+				np = int(p.split(".")[0][-2:1])
+				newList.append(np)
+			except:
+				pass
+		return list(set(newList))
+	elif pacsLevel == 2:
+		newList = []
+		for p in pacsList:
+			try:
+				np = int(p.split(".")[0][-2:2])
+				newList.append(np)
+			except:
+				pass
+		return list(set(newList))
+	else:
+		return pacsList
 
 
 def getYear(row):
-    hist = r['history']
-    if type(hist) is OrderedDict:
-        if 'received' in hist.keys() :                
-            year = int(hist['received'].split("-")[0])            
-            return year
-        else:
-            year = int(hist['published'].split("-")[0])
-            return year
-    else:
-        return 0
+	hist = row['history']
+	if type(hist) is OrderedDict:
+		if 'received' in hist.keys():			 
+			year = int(hist['received']['@date'].split("-")[0])			   
+			return year
+		elif 'published' in hist.keys():
+			year = int(hist['published']['@date'].split("-")[0])
+			return year
+		else:
+			year = int(hist['revised']['@date'].split("-")[0])
+			return year
+	else:
+		return 0
+
+
+def reverseCitingCited(infile, outfile):
+	fIn = open(infile,'r')
+	fOut = open(outfile, 'w')
+	l = fIn.readline().rstrip()
+	while l:
+		li = l.split(",")
+		fOut.write("{!s},{!s}\n".format(li[1],li[0]))
+		l = fIn.readline().rstrip()
+	fIn.close()
+	fOut.close()
+
+
+def dois2ilocs(df):
+	result = {}
+	for index, row in df.iterrows():
+		doi = row['doi']
+		result[doi] = index
+	return result
