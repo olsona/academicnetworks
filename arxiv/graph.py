de2cbfaa5ab531bf9ebd85d5be988d827f77930a
Stefan Pfenninger <stefan@pfenninger.org>
2014-07-02 11:47:34 -0600

    Adjadency dict of dicts for bipartite arxiv graph

--- arxiv/graph.py
+++ arxiv/graph.py
@@ -13,12 +13,17 @@ def get_adjacency_and_weights(df, what='authors', author_initials_only=False,
     """
     Returns a tuple of two things:
 
-    (1) an adjacency list as a dict of dicts from a ``df`` containing
-    arxiv data, and
+    (1) an adjacency list as a dict of dicts from the dataframe ``df``
+    containing arxiv data, in the form (assuming 'name1' authored one
+    paper with 'name2' and two papers with 'name3':
 
-    (2) a dict of node weiths with nodes as keys and weights as values
+    {'name1': {'name2': 1, 'name3': 2}}
 
-    Args:
+    (2) a dict of node weights with nodes as keys and weights as values
+    (the node weights are the total publications for the given author
+    or category)
+
+    Arguments:
 
     ``what`` : 'authors' or 'categories'.
 
@@ -58,14 +63,54 @@ def get_adjacency_and_weights(df, what='authors', author_initials_only=False,
     return (result, weights)
 
 
-def create_graph(df, what):
-    adjacency, weights = get_adjacency_and_weights(df, what=what)
+def get_adjacency_and_weights_bipartite(df, author_initials_only=False,
+                                        subset_categories=None):
+    result = {}
+    weights = {}
+    authorlist = []
+    for index, row in df.iterrows():
+        auths = arxiv.get_authors(row, author_initials_only,
+                                  subset_categories)
+        cats = arxiv.get_categories(row, subset_categories)
+        if auths and cats:  # Skip this row if items is None
+            # Add to node weights dict (`weights`)
+            for a in auths:
+                if a in weights:
+                    weights[a] += 1
+                else:
+                    weights[a] = 1
+                    authorlist.append(a)
+            for c in cats:
+                if c in weights:
+                    weights[c] += 1
+                else:
+                    weights[c] = 1
+            # Add to adjacency list (`result`)
+            for a in auths:
+                if a not in result:
+                    result[a] = {}
+                    for c in cats:
+                        result[a][c] = {'weight': 1}
+            else:
+                for c in cats:
+                    if c not in result[a]:
+                        result[a][c] = {'weight': 1}
+                    else:
+                        result[a][c]['weight'] += 1
+    return (result, weights, authorlist)
+
+
+def create_graph(df, what, initials_only):
+    r = get_adjacency_and_weights(df, what=what,
+                                  author_initials_only=initials_only)
+    adjacency, weights = r
     G = nx.from_dict_of_dicts(adjacency)
     nx.set_node_attributes(G, 'weight', weights)
     return G
 
 
-def create_graphs(df, what='authors', year_range=None, window=5):
+def create_graphs(df, what='authors', year_range=None, window=5,
+                  initials_only=False):
     """
     Returns either a single graph (if year_range is None) or a dict of graphs
     over the sliding window length of ``window`` years, with start years as
@@ -86,7 +131,7 @@ def create_graphs(df, what='authors', year_range=None, window=5):
         graphs = {}
         for year in range(year_range[0], year_range[1] - window + 1):
             graph_df = df[(df.year >= year) & (df.year < year + window)]
-            graphs[year] = create_graph(graph_df, what)
+            graphs[year] = create_graph(graph_df, what, initials_only)
         return graphs
     else:
         return create_graph(df, what)
