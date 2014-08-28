679d5bc2f56c762521854a0a08f282dc2cd78b67
Stefan Pfenninger <stefan@pfenninger.org>
2014-08-28 14:13:41 +0100

    arxiv: functionality to get extended author data

--- arxiv/arxiv.py
+++ arxiv/arxiv.py
@@ -7,6 +7,7 @@ Functions to process and analyze arXiv data
 import glob
 import os
 
+import numpy as np
 import pandas as pd
 
 
@@ -73,24 +74,70 @@ def get_categories(row, subset_categories=None):
     return cats
 
 
-def get_author_series(df, initials_only=False):
+def get_author_series(df, initials_only=False, extended_info=False):
     """
     Return a pandas Series with author names as the index, containing
     total papers per author
 
+    If ``extended_info`` is True, returns a pandas DataFrame instead
+    with the following additional columns:
+        * published: papers that were published (have a DOI)
+        * mean_sub_time: mean time from record creation in arXiv
+          till publication in a journal
+        * 2013 IPP, 2013 SJR, 2013 SNIP: mean 2013 impact factors from
+          journals of all papers published by the author
+
     """
     author_counter = {}
+    published = {}
+    sub_time = {}
+    ipp_curr = {}
+    sjr_curr = {}
+    snip_curr = {}
+
+    CURR = '2013'
+
+    def _increase(d, a):
+        if a in d:
+            d[a] += 1
+        else:
+            d[a] = 1
+
+    def _add(d, s, what):
+        if a in d:
+            d[a].append(what)
+        else:
+            d[a] = [what]
 
     for index, row in df.iterrows():
         authors = get_authors(row, initials_only=initials_only)
         if authors:
             for a in authors:
-                if a in author_counter:
-                    author_counter[a] += 1
-                else:
-                    author_counter[a] = 1
+                _increase(author_counter, a)
+            if extended_info:
+                for a in authors:
+                    if not pd.isnull(row['doi']):
+                        _increase(published, a)
+                    _add(sub_time, a, row['till_published'])
+                    _add(ipp_curr, a, row[CURR + ' IPP'])
+                    _add(sjr_curr, a, row[CURR + ' SJR'])
+                    _add(snip_curr, a, row[CURR + ' SNIP'])
 
-    return pd.Series(author_counter)
+
+    count = pd.Series(author_counter)
+
+    if extended_info:
+        result = pd.DataFrame({'total': count})
+        result['published'] = pd.Series(published)
+        result['published'] = result['published'].fillna(0)
+        result['till_published'] = pd.Series(sub_time).map(np.nanmean)
+        result[CURR + ' IPP'] = pd.Series(ipp_curr).map(np.nanmean)
+        result[CURR + ' SJR'] = pd.Series(sjr_curr).map(np.nanmean)
+        result[CURR + ' SNIP'] = pd.Series(snip_curr).map(np.nanmean)
+    else:
+        result = count
+
+    return result
 
 
 def get_all_authors(df, initials_only=False):
